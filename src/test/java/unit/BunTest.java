efc694ecc20381858074e9865902965d816f863e
Maria Ukleeva <maria.ukleeva@gmail.com>
2023-05-29 22:49:09 +0300

    Исправление шпиона в BurgerTest, добавлена параметризация в BunTest, исправлена версия Java

--- src/test/java/unit/BunTest.java
+++ src/test/java/unit/BunTest.java
@@ -3,26 +3,48 @@ package unit;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import praktikum.Bun;
 
+@RunWith(Parameterized.class)
 public class BunTest {
     private Bun bun;
 
+    private final String name;
+    private final float price;
+    public BunTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+    @Parameterized.Parameters
+
+    public static Object[][] getData() {
+        return new Object[][]{
+                {"Ржаная", -10.0F},
+                {"Rye", 20.0F},
+                {"", 0}
+        };
+    }
+
+
     @Before
     public void createBun(){
-        bun = new Bun("ржаная", 20.01F);
+        bun = new Bun(name, price);
     }
 
     @Test
     public void getNameShouldReturnName(){
 
         String actualName = bun.getName();
-        Assert.assertEquals("ржаная", actualName);
+        Assert.assertEquals(name, actualName);
     }
 
     @Test
     public void getPriceShouldReturnPrice(){
         float actualPrice = bun.getPrice();
-        Assert.assertEquals(20.01F, actualPrice, 0);
+        Assert.assertEquals(price, actualPrice, 0);
     }
 }
--- src/test/java/unit/BurgerTest.java
+++ src/test/java/unit/BurgerTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
@@ -21,7 +20,6 @@ public class BurgerTest {
 Ingredient ingredient;
 @Mock
 Bun bun;
-@Spy
 Burger burger;
 
 @Before
@@ -51,14 +49,14 @@ public void setBunsShouldSetBun(){
 
     }
 
-    //Переделать
+
     @Test
     public void moveIngredientShouldMoveIngredients(){
-        Ingredient ingredient1 = new Ingredient(IngredientType.FILLING, "котлета", 50F);
+        Ingredient secondIngredient = new Ingredient(IngredientType.FILLING, "котлета", 50F);
         burger.addIngredient(ingredient);
-        burger.addIngredient(ingredient1);
+        burger.addIngredient(secondIngredient);
         burger.moveIngredient(1,0);
-        Assert.assertEquals(ingredient1, burger.ingredients.get(0));
+        Assert.assertEquals(secondIngredient, burger.ingredients.get(0));
 
     }
 
@@ -74,14 +72,16 @@ public void setBunsShouldSetBun(){
 
     @Test
     public void getReceiptShouldReturnReceipt(){
-        burger.setBuns(bun);
-        burger.addIngredient(ingredient);
+        Burger spyBurger = Mockito.spy(burger);
+        spyBurger.setBuns(bun);
+        spyBurger.addIngredient(ingredient);
         Mockito.when(bun.getPrice()).thenReturn(20F);
         Mockito.when(bun.getName()).thenReturn("ржаная булочка");
         Mockito.when(ingredient.getPrice()).thenReturn(100F);
         Mockito.when(ingredient.getName()).thenReturn("кетчуп");
         Mockito.when(ingredient.getType()).thenReturn(IngredientType.SAUCE);
-        String receipt = burger.getReceipt();
+        String receipt = spyBurger.getReceipt();
+        Mockito.verify(spyBurger, Mockito.times(1)).getPrice();
         Assert.assertTrue(receipt.contains("(==== ржаная булочка ====)\r\n= sauce кетчуп =\r\n(==== ржаная булочка ====)"));
         Assert.assertTrue(receipt.contains("Price: 140,000000"));
     }
